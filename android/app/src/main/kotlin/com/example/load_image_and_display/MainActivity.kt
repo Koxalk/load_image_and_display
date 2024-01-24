b3b68cc415c10980d83e2de44766d1c98e87fa16
Koxalk <xalkiadakis1994@gmail.com>
2024-01-24 12:07:57 +0200

    final update

---
+++ android/app/src/main/kotlin/com/example/load_image_and_display/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.load_image_and_display
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/mainPage/home_page.dart
@@ -0,0 +1,74 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  MyHomePageState createState() => MyHomePageState();
+}
+
+class MyHomePageState extends State<MyHomePage> {
+  File? _image;
+
+  Future<void> getImage(ImageSource source) async {
+    final pickedFile = await ImagePicker().pickImage(source: source);
+
+    if (pickedFile != null) {
+      setState(() {
+        _image = File(pickedFile.path);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Load Image'),
+      ),
+      body: SafeArea(
+        child: Center(
+            child: _image == null
+                ? const Text('No image selected.')
+                : Image.file(_image!)),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          showModalBottomSheet(
+              context: context,
+              builder: (BuildContext context) {
+                return SafeArea(
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      ListTile(
+                        leading: const Icon(Icons.camera),
+                        title: const Text('Camera'),
+                        onTap: () => {
+                          getImage(ImageSource.camera),
+                          Navigator.pop(context)
+                        },
+                      ),
+                      ListTile(
+                        leading: const Icon(Icons.image),
+                        title: const Text('Gallery'),
+                        onTap: () => {
+                          getImage(ImageSource.gallery),
+                          // dismiss the modal sheet
+                          Navigator.pop(context)
+                        },
+                      ),
+                    ],
+                  ),
+                );
+              });
+        },
+        tooltip: 'New Image',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
